4ad887d50cf3e3fb92d916b35504d881ab2d9e0b
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2018-04-17 16:59:52 +0200

    add to DB OK, problème de rate limit

---
+++ twitter/townhalls_added_to_db.rb
@@ -0,0 +1,61 @@
+require 'rubygems'
+require 'pry'
+require 'csv'
+require 'twitter'
+require 'dotenv'
+
+Dotenv.load("../.env")
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['TWITTER_CONSUMER']
+  config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+  config.access_token        = ENV['TWITTER_TOKEN']
+  config.access_token_secret = ENV['TWITTER_TOKEN_SECRET']
+end
+
+# topics = ["coffee", "tea"]
+# client.filter(track: topics.join(",")) do |object|
+#   puts object.text if object.is_a?(Twitter::Tweet)
+# end
+
+url = '../database/townhalls.csv'
+
+def go_through_all_the_lines(url)
+  list = CSV.read(url)
+end
+
+def handle_search(list)
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV['TWITTER_CONSUMER']
+    config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+    config.access_token        = ENV['TWITTER_TOKEN']
+    config.access_token_secret = ENV['TWITTER_TOKEN_SECRET']
+  end
+
+  list.each do |line|
+    if line[0] == "name"
+    elsif client.user_search("ville #{line[0]}").first
+      line[3] = client.user_search("ville #{line[0]}").first.screen_name
+    else
+      line[3] = ""
+    
+    end
+  end
+  p list
+  return final_list = list
+end
+
+def to_csv(final_list)
+  CSV.open("../database/townhalls.csv", "wb") do |csv|
+    csv << ["name", "zipcode", "email", "handle"]
+    final_list.each do |x|
+    csv << x
+    end
+    end
+  puts "Base de données mise à jour !!"
+end
+
+
+list = go_through_all_the_lines(url)
+final_list = handle_search(list)
+to_csv(final_list)
