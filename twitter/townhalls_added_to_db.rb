750a6642c9cf334459a27fb563213bcb65d20cb2
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2018-04-17 18:10:58 +0200

    Ajout de commentaires sur module Twitter

--- twitter/townhalls_added_to_db.rb
+++ twitter/townhalls_added_to_db.rb
@@ -8,30 +8,34 @@ Dotenv.load("../.env")
 
 url = '../database/townhalls.csv'
 
+  # On appelle notre db et on la stocke dans un tableau.
 def go_through_all_the_lines(url)
   list = CSV.read(url)
 end
 
+  # On recherche le handle de chaque mairie.
 def handle_search(list)
+  # Connexion à l'API Twitter avec le Token renseigné dans le .env loadé.
   client = Twitter::REST::Client.new do |config|
     config.consumer_key        = ENV['TWITTER_CONSUMER']
     config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
     config.access_token        = ENV['TWITTER_TOKEN']
     config.access_token_secret = ENV['TWITTER_TOKEN_SECRET']
   end
-  list.each do |line|
-    if line[0] == "name"
-    elsif client.user_search("ville #{line[0]}").first
+  list.each do |line| # Pour chaque ligne du tableau :
+    if line[0] == "name" # On skip la première.
+    elsif client.user_search("ville #{line[0]}").first # Si on trouve un résultat on l'enregistre 4è valeur du tableau.
       line[3] = client.user_search("ville #{line[0]}").first.screen_name
-    else
+    else # Si on ne trouve rien on enregistre un blanc.
       line[3] = ""
-    break if line[0] == "BUDOS"
+    break if line[0] == "BUDOS" # Pour pouvoir effectuer le test arrête à la mairie BUDOS, sinon on finira par se faire kick de l'API Twitter...
     end
   end
   list
   return final_list = list
 end
 
+  # On rappelle notre db pour enregistrer les handles trouvés.
 def to_csv(final_list)
   CSV.open("../database/townhalls.csv", "wb") do |csv|
     csv << ["name", "zipcode", "email", "handle"]
@@ -42,6 +46,7 @@ def to_csv(final_list)
   puts "Base de données mise à jour !!"
 end
 
+  # Perform des trois méthodes.
 def perform_handler()
   list = go_through_all_the_lines(url)
   final_list = handle_search(list)
--- twitter/townhalls_follower.rb
+++ twitter/townhalls_follower.rb
@@ -8,25 +8,29 @@ Dotenv.load("../.env")
 
 url = '../database/townhalls.csv'
 
+  # On appelle notre db et on la stocke dans un tableau.
 def go_through_all_the_lines(url)
   list = CSV.read(url)
 end
 
+  # On follow chaque handle de notre db.
 def townhall_follower(list)
+  # Connexion à l'API Twitter avec le Token renseigné dans le .env loadé.
   client = Twitter::REST::Client.new do |config|
     config.consumer_key        = ENV['TWITTER_CONSUMER']
     config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
     config.access_token        = ENV['TWITTER_TOKEN']
     config.access_token_secret = ENV['TWITTER_TOKEN_SECRET']
   end
-  list.each do |line|
-    if line[3] == "" || line[3] == "handle"
+  list.each do |line| # Pour chaque ligne du tableau :
+    if line[3] == "" || line[3] == "handle" # On skip la première ligne, ou celles qui sont vides.
     else
-      client.follow("#{line[3]}")
+      client.follow("#{line[3]}") # On follow les handles trouvés précédemment.
     end
   end
 end
 
+  # Perform des deux méthodes.
 def perform_follower()
   townhall_follower(go_through_all_the_lines(url))
 end
