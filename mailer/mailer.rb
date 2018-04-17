37c4b6e4a16c78454b87b4cc3956b1324bc45be6
iMassim0 <i.max@live.fr>
2018-04-17 14:33:22 +0200

    MAJ mailer fonctionnel

--- mailer/mailer.rb
+++ mailer/mailer.rb
@@ -3,6 +3,7 @@ require 'dotenv'
 Dotenv.load('../.env')
 require 'CSV'
 
+# Tri du CSV appelé dans l'URL, et retourne un array propre pour traitement dans d'autre fonction
 def go_through_all_the_lines(url)
 
   townhalls_list = CSV.read(url)
@@ -18,29 +19,36 @@ def go_through_all_the_lines(url)
 
 end
 
-def get_the_email_html()
-
-  message = File.open("message.html.erb")
+#
+def get_the_email_html(line)
+  @mairie = line[0]
+  message = File.read("message.html.erb")
   return message
 
 end
 
-def send_email_to_line(list)
+def send_email_to_line(line)
 
+
+  # Appel du message dans la fonction envoyer email
+  content = get_the_email_html(line)
   # Authentification session with .env
-
   gmail = Gmail.connect(ENV['USERNAME'], ENV['PASSWORD'])
-
-  return
-
+  # Envoi du mail à l'email
+  gmail.deliver do
+    to "#{line[2]}"
+    subject "Promo de THP spécialement pour la Mairie de #{line[0]}!"
+    html_part do
+      content_type 'text/html; charset=UTF-8'
+      body content
+    end
+  end
 end
 
 def perform
   townhalls_lines = go_through_all_the_lines('temp_list.csv')
 
   # townhalls_lines.each do |line|
-
-
 
   # while i < townhalls_lines.length do
   #   townhalls_emails << townhalls_lines[i][2]
@@ -49,9 +57,10 @@ def perform
 
   # return townhalls_emails
 
-  get_the_email_html()
 
+
+  send_email_to_line(townhalls_lines[1])
 
 end
 
-print perform()
+perform()
