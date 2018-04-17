80a2485b243904b021afed01b4cfbb4c4f523571
Baptiste Rogeon <baptiste.rogeon@gmail.com>
2018-04-17 12:41:43 +0200

    scrapper terminé ! =)

--- scrapper/townhalls_scrapper.rb
+++ scrapper/townhalls_scrapper.rb
@@ -28,20 +28,32 @@ end
 
 def get_the_email_of_a_townhall_from_its_webpage(url)
   townhall_hash = {}
+  final_array = []
   url.each do |url|
     page = Nokogiri::HTML(open(url))
     name = page.css("/html/body/div/main/section[1]/div/div/div/h1").text.gsub(" - ","").gsub!(/[0-9]/, "")
     zipcode = url.gsub!(/\D/, "")
     email = page.css("/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]").text
     townhall_hash = { :name => name, :zipcode => zipcode, :email => email }
+    final_array << townhall_hash
   end
-   return townhall_hash
+  return final_array
+end
+
+def to_csv(final_array)
+  CSV.open("../database/townhalls.csv", "wb") do |csv|
+    csv << final_array.first.keys
+    final_array.each do |x|
+    csv << x.values
+    end
+    end
 end
 
 
 def perform(dept)
   # get_the_email_of_a_townhall_from_its_webpage(get_number_of_pages(dept))
-  get_the_email_of_a_townhall_from_its_webpage(get_all_the_urls_of_townhalls(get_number_of_pages(dept)))
+  final_array = get_the_email_of_a_townhall_from_its_webpage(get_all_the_urls_of_townhalls(get_number_of_pages(dept)))
+  to_csv(final_array)
 end
 
  p perform("gironde")
