688e62d26e79892fbf27456d64fe9dbb53ce86db
iMassim0 <i.max@live.fr>
2018-04-17 18:18:42 +0200

    MAJ & FIN Perform_global.rb

--- perform_global.rb
+++ perform_global.rb
@@ -9,15 +9,18 @@ require 'CSV'
 
 
 def perform_global
-
+  # On appelle la fonction qui lance toute l'acquisition des e-mails des départements souhaités avec l'appel de la fonction
+  # perform_scrapper issu du fichier townhalls_scrapper.rb
   perform_scrapper(["nord", "gironde", "landes"])
-
+  # On appelle la fonction qui lance l'envoi des messages aux e-mails préalablement chargés par le perform_scrapper
+  # Le perform_mailer est issu du fichier mailer.rb
   perform_mailer('database/townhalls.csv', "mailer/message.html.erb")
-
+  # On appelle la fonction qui lance la recherche générale des comptes twitter des villes des départements souahités
+  # Le perform_handler est issu du fichier townhalls_added_to_db
   perform_handler()
-
+  # On appelle la fonction qui fait suivre toutes les personnes qui sont concernées par les comptes twitter précédemment récupérés
+  # Le perform_handler est issu du fichier townhalls_follower
   perform_follower()
-
 end
 
 perform_global()
